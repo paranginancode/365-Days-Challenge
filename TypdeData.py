e12acb620cd943cff88679af4dd904817b23d74c
MiawMeow <47144321+oktodotid@users.noreply.github.com>
2023-04-12 12:30:36 +0700

    Create TypdeData.py

---
+++ TypdeData.py
@@ -0,0 +1,55 @@
+# String (tipe data : str) adalah Karakter Kata
+# Contoh : 
+
+print('Belajar Python')
+print('Python Itu Mudah')
+
+# Integer (tipe data : int) adalah Bilangan Bulat tanpa tanda Decimal
+# Contoh : 
+
+print(20)
+print(100)
+
+# Float (tipe data : float) adalah Bilangan Decimal
+#Contoh : 
+
+print(21.5)
+print(23.45)
+
+# List (tipe data : list) adalah Kumpulan Data atau Objek
+#Contoh :
+
+[1,'23', 'Python', 4.5]
+
+#Dictionaries (tipe data : dict) adalah objek berpasangan
+#Contoh :
+
+dict = {'Nama':'Python', 'Umur': 23, 'Alamat':'Jakarta'}
+dict2 = {'item1' : 'ayam', 'item2' : 'ikan', 'item3' : 'sapi'}
+print(dict)
+print(dict2)
+
+#Tuples (tipe data : tup) adalah kumpulan data yang tidak dapat diubah
+#Contoh :
+
+tup = ('Python', 'Java', 'C++')
+tup2 = (1,'23.3','belajar', 4.5)
+print(tup)
+print(tup2)
+
+#Set (tipe data : set) adalah kumpulan data yang tidak memiliki urutan dan tidak dapat diubah
+#Contoh :
+
+set = {'Python', 'Java', 'C++'}
+set2 = {'apel', 'jeruk', 'mangga'}
+print(set)
+print(set2)
+
+# Boolean (tipe data : bool) adalah tipe data yang hanya memiliki dua nilai yaitu True atau False
+#Contoh :
+
+a = True
+b = False
+
+print(a)
+print(b)
